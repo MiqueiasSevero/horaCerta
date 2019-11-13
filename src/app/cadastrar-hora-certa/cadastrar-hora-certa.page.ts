1eef357f8a068a90ddf9a784103d612aa98adbdf
Miqueias Severo <miq.severo@gmail.com>
2019-11-12 22:20:21 -0300

    gravar data e hora no storage e mostrar na home

--- src/app/cadastrar-hora-certa/cadastrar-hora-certa.page.ts
+++ src/app/cadastrar-hora-certa/cadastrar-hora-certa.page.ts
@@ -29,9 +29,53 @@ export class CadastrarHoraCertaPage implements OnInit {
   ngOnInit() {
   }
 
+  formataZerosEsquerdas(valor:number){
+    return valor > 10 ? valor :"0" +valor;
+  }
+
   cadastrarHoraCerta(){
 
-  console.log( this.formCadHoraCerta.value);
+    
+
+    
+    let form = this.formCadHoraCerta.value;
+    form.status = 0;
+    
+    let dataCompleta =  new Date(form.data), horaCompleta =  new Date(form.hora);
+
+
+    let dia = this.formataZerosEsquerdas(dataCompleta.getDate()),
+        mes = this.formataZerosEsquerdas(dataCompleta.getMonth() + 1 ),
+        ano = this.formataZerosEsquerdas(dataCompleta.getFullYear()),
+        hora = this.formataZerosEsquerdas(horaCompleta.getHours()),
+        minutos = this.formataZerosEsquerdas(horaCompleta.getMinutes());
+
+
+        form.data = `${dia} / ${mes} / ${ano}`;
+        form.hora = `${hora}:${minutos}`;
+        console.log( form);
+
+
+
+
+
+
+
+    let listaHoraCerta = [form];
+
+  this.storage.get('listaHoraCerta').then((value:any) => {
+    if (value !== null || value !== undefined) {
+      let objeto = JSON.parse(value);
+      listaHoraCerta = listaHoraCerta.concat(objeto);
+    }
+       this.storage.set('listaHoraCerta', JSON.stringify(listaHoraCerta));
+    });
+  
+  
+  
+  
+  
+    
 
   }
 
